4a4bf21326cc6b0fee59fc8c64b35e4b32ed717d
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-24 21:23:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from chatterbot import ChatBot
+import time
+from chatterbot.trainers import ListTrainer
+
+# time error
+time.clock = time.time
+
+chatbot = ChatBot("Panagiotis")
+
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+    "Geia",
+    "Pos eisai re seira",
+    "Mia xara esu",
+    "kai egw kala",
+    "fantastic",
+    "spectacular",
+])
+
+
+trainer.train([
+    "i want to learn more",
+    "You can learn more about as in www.....etc",
+])
+
+
+print('Type something to begin...')
+
+# response = chatbot.get_response("Hey")
+# print(response)
+
+# begin testing
+while True:
+    try:
+        user_input = input()
+
+        bot_response = chatbot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
